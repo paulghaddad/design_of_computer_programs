6b89a592dff30a3ddf6c8cf1d5b3df024f4aff70
Paul Haddad <paulh16@gmail.com>
2020-10-12 14:44:27 -0400

    L1 - 9 - Testing for Extreme Values

--- lesson1/test_poker.py
+++ lesson1/test_poker.py
@@ -10,9 +10,13 @@ def test_poker():
     fh = "TD TC TH 7C 7D".split()
     assert poker([sf, fk, fh]) == sf
 
-    # Add 2 new assert statements here. The first
-    # should check that when fk plays fh, fk 
-    # is the winner. The second should confirm that
-    # fh playing against fh returns fh.
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
+
+    # Add 2 new assert statements here. The first 
+    # should assert that when poker is called with a
+    # single hand, it returns that hand. The second 
+    # should check for the case of 100 hands.
+    assert poker([fh]) == fh
+    assert poker(100 * [fh]) == fh
+
