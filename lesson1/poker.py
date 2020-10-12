9ada7ae2ca1b406217c91132a42f5cb2e0628dc4
Paul Haddad <paulh16@gmail.com>
2020-10-12 14:52:19 -0400

    L1 - 10 - First pass at implementing hand_rank function

--- lesson1/poker.py
+++ lesson1/poker.py
@@ -2,6 +2,12 @@ def poker(hands):
     "Return the best hand: poker([hand,...]) => hand"
     return max(hands, key=hand_rank)
 
-def hand_rank(hand):
-    # will be implemented later on, for now assume it provides a rank given a hand
-    pass
+  def hand_rank(hand):
+    "Return a value indicating the ranking of a hand."
+    # Extract ranks from the hand  
+    ranks = card_ranks(hand)
+    # Check if it's a straight flush
+    if straight(ranks) and flush(ranks):
+        return 8 # there are 9 possible levels of poker hands, so return 0-8
+    elif kind(4, ranks):
+        return 7
